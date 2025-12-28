0f66b397999cc1fb1d39e26382833bfaeeff4322
Rohit <rohitmahatoece@gmail.com>
2025-12-28 12:02:32 +0530

    basics of js

---
+++ day01/variablesAndDatatypes.js
@@ -0,0 +1,65 @@
+// let name = "rohit";
+// let age = 20;
+// age = 30;
+// console.log(name, age);
+
+// const account = 1234;
+//account = 23;
+//console.log(account);
+
+//primitive datatypes;
+
+//number
+
+// let a = 20;
+// let c = 30;
+// console.log(a, c);
+
+//string
+
+// let c = "rohit";
+// const d = "mahato";
+// console.log(c, d);
+
+//boolean
+
+// let login = true;
+// console.log(login);
+
+//undefine
+
+// let user;
+// console.log(user);
+//agr user me koi bhi value assign nhi tb undefined print karega.
+
+//bigint
+
+// let num = 12365745487268n;
+// console.log(num);
+//koi badi value assign karna ho tb.
+
+//non primitive datatypes
+
+// let arr = [10,20,11,"rohit",true];
+// console.log(arr);
+
+// let user = {
+//     name:"rohit",
+//     account:1234,
+//     age:20
+// }
+// console.log(user);
+
+//function
+
+// function add(){
+//     console.log("hello");
+// }
+// add();
+
+//or function ko obj ke ander bhi store kar sktr hai.
+
+// let obj = function(){
+//     console.log("hello");
+// }
+// obj();
