652af0e604eb11f0b8e154ed4a3844462456451c
Rohit <rohitmahatoece@gmail.com>
2025-12-29 12:50:45 +0530

    frist commit

---
+++ day01/numberAndMathsObject.js
@@ -0,0 +1,15 @@
+//number
+
+// let a = 10;
+// let b = 345.785;
+// // let c = b.toFixed(2);
+// // console.log(typeof c);
+
+// console.log(b.toPrecision(4));
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.SQRT2);
+console.log(Math.random());
