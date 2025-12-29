620618c27f5621b06b29142ca3157245539c9713
Rohit <rohitmahatoece@gmail.com>
2025-12-30 04:51:22 +0530

    frist commit

---
+++ day01/stringAndDates.js
@@ -0,0 +1,69 @@
+// const str1 = "rohit";
+// const str2 = 'rohit mahato';
+// const str3 = `hello world`;
+// console.log(str1,str2,str3);
+
+// const day = 18;
+// const str3 = `hello world ${day}`;
+// console.log(str3);
+
+// const str = `hello coder army`;
+// console.log(str.length);
+// console.log(str[1]);
+
+
+//uppercase
+
+// const str = `hello coder army`;
+// console.log(str.toUpperCase());
+
+// to find index
+
+// const str = `hello coder army`;
+// console.log(str.indexOf(`cod`));
+
+//to find las4t index
+
+// const str = `hello coder army`;
+// console.log(str.lastIndexOf(`cod`));
+
+// include matlb presen hai ya nhi
+
+// const str = `hello coder army`;
+// console.log(str.includes(`cod`));
+
+//slice
+
+// const str = `hello coder army`;
+// console.log(str.slice(2,7));
+// console.log(str.slice(2));
+// console.log(str.slice(-4));
+
+//subString
+
+// const str = `hello coder army coder`;
+// console.log(str.substring(2,5));
+
+//replace
+
+// const str = `hello coder army coder`;
+// console.log(str.replace("ode",`iam`));
+// console.log(str.replaceAll("ode",`iam`));
+
+//trim
+
+// const user = " Rohit mahato ";
+// console.log(user.trim());
+
+//split
+
+// const user = "Rohit,Mohit,Rohan,Suraj";
+// console.log(user.split(","));
+
+//dates
+
+const now = new Date();
+//console.log(now.toString());
+//console.log(now.toLocalString());
+ console.log(now.getDay());
+console.log(now.getMonth());
