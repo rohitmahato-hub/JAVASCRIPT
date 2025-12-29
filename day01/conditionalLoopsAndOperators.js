8994910414dbb70aed80a594e6e8f47f0ab1540a
Rohit <rohitmahatoece@gmail.com>
2025-12-29 10:02:38 +0530

    frist commit

---
+++ day01/conditionalLoopsAndOperators.js
@@ -0,0 +1,88 @@
+//operators
+
+// console.log(5+5);
+// console.log(5-5);
+// console.log(5*5);
+// console.log(5%5);
+// console.log(5/5);
+
+
+// assignment operators
+
+// let x = 30;
+// let y = 20;
+// x = x+y;
+// x+=y;  assignment operator bolte hai.
+// x-=y;
+// x*=y;
+// console.log(x);
+
+//Comparison operators
+
+// console.log(5>6);
+// console.log(5<6);
+// console.log(5>=6);
+// console.log(5>=6);
+
+// console.log(null==0);
+// console.log(null===undefined);
+
+//loops
+
+// # for loops
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+//# do while loops
+
+// let i = 0;
+// do{
+//     console.log(i);
+//     i++;
+// }while(i<10);
+
+
+//# while loops
+
+// let i =0;
+// while(i<10){
+//     console.log(i);
+//     i++;
+// }
+
+//conditional statements
+
+// if eles condition
+
+// let age = 15;
+// if(age>= 18){
+//     console.log("eligible for vote");
+// }else{
+//      console.log("not eligible for vote");
+// }
+
+
+// let age = 20;
+// if(age<18){
+//     console.log("kid");
+// }else if(age>=50){
+//     console.log("old");
+// }else{
+//   console.log("young");  
+// }
+
+
+//logical operators( &&, ||, !=)
+
+// console.log(true && true);
+// console.log(true && false);
+// console.log(false && true);
+// console.log(false && false);
+
+
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
