6746ca18ab4f6ae85a1c85df8fe9ea680c16d7e7
Rohit <rohitmahatoece@gmail.com>
2026-02-18 19:30:48 +0530

    first commit

---
+++ practice/dom2/code.js
@@ -0,0 +1,10 @@
+let newBtn = document.createElement("button");
+newBtn.innerText = "click me!";
+
+newBtn.style.color = "white";
+newBtn.style.backgroundColor = "red";
+
+document.querySelector("body").prepend(newBtn);
+
+//q2
+
