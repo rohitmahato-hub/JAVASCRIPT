99914071974b1f1343207dd66e9608d2122e0bf8
Rohit <rohitmahatoece@gmail.com>
2026-02-18 10:50:25 +0530

    first commit

--- practice/fifth.js
+++ practice/fifth.js
@@ -23,16 +23,16 @@
 
 //question
 
- function countVowels(str){
-    let count = 0;
-    for(const char of str){
-        if(char === "a" || char === "e" || char ==="e" || char === "i" || char === "o" || char === "u"){
-            count++;
-        }
-    }
-    console.log(count);
- }
- countVowels("rohit");
+//  function countVowels(str){
+//     let count = 0;
+//     for(const char of str){
+//         if(char === "a" || char === "e" || char ==="e" || char === "i" || char === "o" || char === "u"){
+//             count++;
+//         }
+//     }
+//     console.log(count);
+//  }
+//  countVowels("rohit");
 
 //arroe function
  let countVowels = (str) =>{
